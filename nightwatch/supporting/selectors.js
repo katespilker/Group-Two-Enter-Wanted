225486da975a5007a4ceb98188a1c7df2d1dcaf9
Kate Spilker <katespilker7@gmail.com>
2018-02-07 13:33:27 -0700

    selectors added

---
+++ nightwatch/supporting/selectors.js
@@ -0,0 +1,71 @@
+module.exports = {
+
+    paragrahHeadings: {
+        titleBar: 'h1[class="titleBar]',
+        subHeader: 'td=[colspan="2"]',
+        hdrHeader: 'span[name="hdrHeader"]',
+        mkeHeader: 'span[name="mkeHeader"]',
+        oriHeader: 'span[name="oriHeader"]',
+        namHeader: 'span[name="namHeader"]',
+        sexHeader: 'span[name="sexHeader"]',
+        racHeader: 'span[name="racHeader"]',
+        hgtHeader: 'span[name="hgtHeader"]',
+        wgtHeader: 'span[name="wgtHeader"]',
+        haiHeader: 'span[name="haiHeader"]',
+        offHeader: 'span[name="offHeader"]',
+        dowHeader: 'span[name="dowHeader"]',
+        olnHeader: 'span[name="olnHeader"]',
+        olsHeader: 'span[name="olsHeader"]',
+        oldHeader: 'span[name="oldHeader"]',
+        licHeader: 'span[name="licHeader"]',
+        lisHeader: 'span[name="lisHeader"]',
+        lidHeader: 'span[name="lidHeader"]',
+        widHeader: 'span[name="widHeader"]',
+        resHeader: 'span[name="resHeader"]',
+        datHeader: 'span[name="datHeader"]',
+        queryTitle: 'span[name"queryTitle"]',
+        errorHeader: 'span[id="validHeader"]',
+        errorMessage: 'li[class="errorMessage"]',
+        results: 'span[name"queryTitle"]',
+        queryBody: 'span[name="queryBody"]',
+
+    },
+
+    fields: {
+        hdrInput: 'input[name=hdrInput"]',
+        mkeInput: 'input[name=mkeInput"]',
+        oriInput: 'input[name=oriInput"]',
+        namInput: 'input[name=namInput"]',
+        sexInput: 'input[name=sexInput"]',
+        racInput: 'input[name=racInput"]',
+        hgtInput: 'input[name=hgtInput"]',
+        wgtInput: 'input[name=wgtInput"]',
+        haiInput: 'input[name=haiInput"]',
+        offInput: 'input[name=offInput"]',
+        dowInput: 'input[name=dowInput"]',
+        olnInput: 'input[name=olnInput"]',
+        olsInput: 'input[name=olsInput"]',
+        oldInput: 'input[name=oldInput"]',
+        licInput: 'input[name=licInput"]',
+        lisInput: 'input[name=lisInput"]',
+        lidInput: 'input[name=lidInput"]',
+        widInput: 'input[name=widInput"]',
+        resInput: 'input[name=resInput"]',
+        datInput: 'input[name=datInput"]',
+
+    },
+
+    buttons: {
+        menuIcon: 'div[class="bm-burger-button]',
+        crossButton: 'div[class="bm-cross-button]',
+        homeOption: 'p[name="homeOption"]',
+        enterOption: 'p[name="enterOption"]',
+        modifyOption: 'p[name="modifyOption"]',
+        cancelOption: 'p[name="cancelOption"]',
+        clearBtn: 'button[id="clearBtn"]',
+        saveBtn: 'button[id="saveBtn"]',
+
+
+    },
+
+}
